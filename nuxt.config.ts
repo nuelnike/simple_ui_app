a219283a9ec395231cbbf9583f9b921c2963c191
AgbaOgbonna Emmanuel Ikenna <ae21965@gmail.com>
2025-07-27 00:50:00 +0100

    add devServer

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,18 @@
 import { defineNuxtConfig } from "nuxt/config";
+
 export default defineNuxtConfig({
   ssr: false,
   srcDir: "src",
   modules: ["@pinia/nuxt"],
+  css: ["animate.css"],
+  compatibilityDate: "2024-11-20",
+  devtools: { enabled: false },
+
+  devServer: {
+    port: 5000,
+    host: "0.0.0.0", // Required to expose app on public IP (Contabo)
+  },
+
   vite: {
     server: {
       watch: {
@@ -10,24 +20,22 @@ export default defineNuxtConfig({
       },
     },
   },
-  css: ["animate.css"],
+
   app: {
     head: {
-        titleTemplate: `Sample UI`,
-        meta: [
-          { charset: "UTF-8" },
-          { name: "viewport", content: "width=device-width, initial-scale=1.0"}
-        ], 
-        script: [],
-        link: [
-          { href:"/style.css", rel:"stylesheet"},
-        ], 
-        bodyAttrs: {},
-        htmlAttrs: {
-          "lang": "en"
-        }
+      titleTemplate: `Sample UI`,
+      meta: [
+        { charset: "UTF-8" },
+        { name: "viewport", content: "width=device-width, initial-scale=1.0" }
+      ],
+      script: [],
+      link: [
+        { href: "/style.css", rel: "stylesheet" },
+      ],
+      bodyAttrs: {},
+      htmlAttrs: {
+        lang: "en"
+      }
     },
   },
-  compatibilityDate: "2024-11-20",
-  devtools: { enabled: false }
 });
